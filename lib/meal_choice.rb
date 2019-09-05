916cd51ec378dffec67051534353c1ff339284eb
Lialah Barrer <lrbarrer@gmail.com>
2019-09-05 00:03:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 def meal_choice (veg1, veg2, protein = "meat")
-  order = "What a nutritious meal! A plate of #{protein} with #{veg1} and #{veg2}."
+  order = "What a nutritious meal!" + "A plate of #{protein} with #{veg1} and #{veg2}."
   puts order
 end
